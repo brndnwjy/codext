b5e4d4a7a6cd3042aa78a8b660d7fd6c25549c10
Brandon Wijaya <brandon@softwareseni.com>
2023-09-27 09:14:17 +0700

    feat: add color palette to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        lightBlue: "#D4D1E2",
+        primaryBlue: "#2657A4",
+        heading: "#060E1A",
+        text: "#51565E",
+        arrowRed: "#B25D0F",
+        arrowGreen: "#4D9D0E",
+        arrowBlue: "#0E479D",
+        gradientBlueLeft: "#4586E9",
+        gradientBlueRight: "#3036C6",
+      },
     },
   },
   plugins: [],

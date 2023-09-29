e7f21aaaabe093fb7beec65f0cbaa565945cb130
Brandon Wijaya <brandon@softwareseni.com>
2023-09-29 10:55:07 +0700

    feat: add advantage section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import ArrowPoint from "./ArrowPoint";
+import Advantage from "./Advantage";
 
-export { Navbar, Hero, ArrowPoint };
+export { Navbar, Hero, ArrowPoint, Advantage };

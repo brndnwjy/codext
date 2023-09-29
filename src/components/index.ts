71b56a1908d53979232a4723c9250b9c46bc9336
Brandon Wijaya <brandon@softwareseni.com>
2023-09-29 11:34:12 +0700

    feat: add services section

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Hero from "./Hero";
 import ArrowPoint from "./ArrowPoint";
 import Advantage from "./Advantage";
 import ServiceCard from "./ServiceCard";
+import Services from "./Services";
 
-export { Navbar, Hero, ArrowPoint, Advantage, ServiceCard };
+export { Navbar, Hero, ArrowPoint, Advantage, ServiceCard, Services };

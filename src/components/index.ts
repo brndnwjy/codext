a8ffa81041e3711b17714549182625c65ea11d0c
Brandon Wijaya <brandon@softwareseni.com>
2023-09-29 11:33:28 +0700

    feat: add service card component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import ArrowPoint from "./ArrowPoint";
 import Advantage from "./Advantage";
+import ServiceCard from "./ServiceCard";
 
-export { Navbar, Hero, ArrowPoint, Advantage };
+export { Navbar, Hero, ArrowPoint, Advantage, ServiceCard };

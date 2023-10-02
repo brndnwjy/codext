a81901d2c457719b916a85794af20696f15e7f14
Brandon Wijaya <brandon@softwareseni.com>
2023-10-02 10:56:49 +0700

    fix: make page scroll to smooth

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{ scrollBehavior: "smooth" }}>
       <Head />
       <title>Codext</title>
       <link rel="shortcut icon" href="/codext-icon.ico" />
